69f9348488a53707f2744e7038572115faf5cfbe
EFatihAydin <efatih.aydin@gmail.com>
2024-07-11 11:13:01 +0300

    db den gelecek json tamamlandı

--- digibro/questions/views.py
+++ digibro/questions/views.py
@@ -59,7 +59,8 @@ def index(request):
                         \nRisk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her türlü \
                         yardım ve desteğe hazır olduğunu ayrıca belirtiriz.''',
                     'qsdc_check' : False
-                }
+                },
+                "Bilgilendirme Metni'ni okudum ve onaylıyorum." : 'check'
             },
             'Ön Bilgi' : {
                 'default' : {
@@ -105,6 +106,222 @@ def index(request):
                     }
                 },
                 'Olayı Yazınız.' : 'textarea'
+            },
+            'Hasar' : {
+                'default' : {
+                    'qsdc_text' : 'Risk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her \
+                        türlü yardım ve desteğe hazır olduğunu ayrıca belirtiriz.',
+                    'qsdc_check' : True
+                },
+                'Hasar Geçmişi' : {
+                    "select" : ['Hasar Ücreti','Ödendi','Muallak'],
+                    'Hasar Teminatı' : 'input',
+                    "Hasar Bedeli Tutar" : {
+                        "currency": ["TL","USD","EUR"],
+                        "currency_text" : "Para Birimi Seçin"
+                    }
+                },
+                'Hasar Açıklama' : {
+                    'Hasar nedenini yazınız.' : 'textarea',
+                    'İlgili iyileştirmeleri yazınız.' : 'textarea',
+                }
+            },
+            'Yangın Önlemleri' : {
+                'default' : {
+                    'qsdc_text' : 'Risk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her \
+                        türlü yardım ve desteğe hazır olduğunu ayrıca belirtiriz.',
+                    'qsdc_check' : True
+                },
+                'Yangın Tüpleri' : {
+                    "radio" : ["Yangın Tüpleri","Var","Yok"],
+                    'Adet' : 'input'
+                },
+                'Yangın Dolapları' : {
+                    "radio" : ["Yangın Dolapları","Var","Yok"],
+                    'Adet' : 'input'
+                },
+                'Yangın Hidrantları' : {
+                    "radio" : ["Yangın Hidrantları","Var","Yok"],
+                    'Adet' : 'input'
+                },
+                'Duman Dedektörleri' : {
+                    "radio" : ["Duman Dedektörleri","Var","Yok"],
+                    'Tesisin % kaçını kapsıyor?' : 'int'
+                },
+                'Yangın Suyu Deposu' : {
+                    "radio" : ["Yangın Suyu Deposu","Var","Yok"],
+                    'Top' : 'float'
+                },
+                'Yangın Pompa Grubu' : {
+                    "radio" : ["Yangın Pompa Grubu","Elektrikli","Dizel","Dizel-Elektrikli","Yok"],
+                    'Açıklama' : 'input'
+                },
+                "radio" : ["Yangın Pompası İçin Otomatik Jeneratör","Var","Yok"],
+                'Sprinkler Tesisatı' : {
+                    "radio" : ["Sprinkler Tesisatı","Var","Yok"],
+                    'Tesisin % kaçını kapsıyor?' : 'int'
+                },
+                "radio" : ["Lokal Söndürme Sistemleri","Var","Yok"],
+                "Bina Bedeli Tutarı" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                'Bina Alanı (m²)' : 'float',
+                "Demirbaş Bedeli" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                "Dekorasyon Bedeli" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                "Makina Bedeli" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                "Emtia" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                }
+            },
+            'Elektronik Cihaz' : {
+                'default' : {
+                    'qsdc_text' : 'Risk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her \
+                        türlü yardım ve desteğe hazır olduğunu ayrıca belirtiriz.',
+                    'qsdc_check' : True
+                },
+                "Bedel" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                'Model' : 'input',
+                'Marka' : 'input',
+                'Seri No' : 'input',
+                'Yaşı' : 'int',
+            },
+            'Makine Kırılması' : {
+                'default' : {
+                    'qsdc_text' : 'Risk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her \
+                        türlü yardım ve desteğe hazır olduğunu ayrıca belirtiriz.',
+                    'qsdc_check' : True
+                },
+                "Bedel" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                'Model' : 'input',
+                'Marka' : 'input',
+                'Seri No' : 'input',
+                'Yaşı' : 'int',
+            },
+            'İşveren Mali Mesuliyet' : {
+                'default' : {
+                    'qsdc_text' : 'Risk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her \
+                        türlü yardım ve desteğe hazır olduğunu ayrıca belirtiriz.',
+                    'qsdc_check' : True
+                },
+                'Mavi Yaka Çalışan Sayısı' : 'int',
+                'Beyaz Yaka Çalışan Sayısı' : 'int',
+                "Yıllık Brüt İşçilik Ücretleri Toplamı" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                "Ek Olarak İstenen Limit 1. Alternatif" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Limit Para Birimi Seçin"
+                },
+                "Ek Olarak İstenen Limit 2. Alternatif" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Limit Para Birimi Seçin"
+                },
+                "Ek Olarak İstenen Limit 3. Alternatif" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Limit Para Birimi Seçin"
+                },
+            },
+            'Şahıs Mali Mesuliyet' : {
+                'default' : {
+                    'qsdc_text' : 'Risk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her \
+                        türlü yardım ve desteğe hazır olduğunu ayrıca belirtiriz.',
+                    'qsdc_check' : True
+                },
+                "Ciro" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Para Birimi Seçin"
+                },
+                "Ek Olarak İstenen Limit 1. Alternatif" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Limit Para Birimi Seçin"
+                },
+                "Ek Olarak İstenen Limit 2. Alternatif" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Limit Para Birimi Seçin"
+                },
+                "Ek Olarak İstenen Limit 3. Alternatif" : {
+                    "currency": ["TL","USD","EUR"],
+                    "currency_text" : "Limit Para Birimi Seçin"
+                },
+            },
+            'Hırsızlık Önlemleri' : {
+                'default' : {
+                    'qsdc_text' : 'Risk Bilgi Formu hazırlanması sürecinde ve sigorta teminatlarını kapsayacak teknik konularda şirketimizin her \
+                        türlü yardım ve desteğe hazır olduğunu ayrıca belirtiriz.',
+                    'qsdc_check' : True
+                },
+                'radio' : ['Tesis çevresi çevrili mi?','Evet','Hayır'],
+                'radio' : ['Bekçi köpeği var mı?','Var','Yok'],
+                'radio' : ['Çevre Aydınlatması var mı?','Var','Yok'],
+                'radio' : ['Kepenk var mı?','Var','Yok'],
+                'radio' : ['24 saat güvenlik var mı?','Var','Yok'],
+                'radio' : ['Kayıt alabilen kamera sistemi var mı?','Var','Yok'],
+                'radio' : ['Güvenlik şirketi ve/veya karakola bağlı alarm sistemi var mı?','Var','Yok'],
+                'radio' : ['Karakola bağlı alarm sistemi var mı?','Var','Yok'],
+                'Ek tedbirleriniz varsa yazınız.' : 'textarea'
+            },
+            'Fotoğraflar' : {
+                'default' : {
+                    'qsdc_text' : """Risk Değerlendirmesi Çalışması için ihtiyaç duyacağımız fotoğraflar aşağıda maddeler halinde paylaşılmıştır. ilgili fotoğrafların aşağıda bulunan fotoğraf yükleme alanlarınayüklenmesini rica ederiz. Her bir fotoğrafın 1 MI311 geçmemesi dosyanın mail ile paylaşılabilmesi açısından önem arz etmektedir. Gerekli görülmesi halinde fotoğrafların küçültüldükten sonra yüklenmesi uygun olacaktır.
+
+- İşletme içerisinden genel vaziyeti gösterecek şekilde olabildiğince geniş açılı birkaç fotoğraf, - İşletmede bulunan tüm yangın önlemlerini (Yangın tüpleri, yangın dola pları, duman dedektörleri, sprinkler vb.) gösterir şekilde geniş açılı birkaç fotoğraf,
+
+- İşletmede bulunan tüm sel önlemlerini(Palet/raf üzeri depolama, mazgallar, su toplama kuyuları, menfezler vb.) gösterir şekil-de geniş açılı birkaç fotoğraf,
+
+- İşletmede bulunan tüm hırsızlık önlemlerini(kepenk, kamera sistemleri, hırsızlık alarmları vb.) gösterir şekilde geniş açılı birkaç fotoğraf,
+
+- İşletme çevresinden binayı ve çevresini görebileceğimiz şekilde geniş açılı birkaç fotoğraf, - Yangın pompa grubu, pompa basınç göstergesi, yangın kollektörü ve yangın suyu deposundann geniş açılı birkaç fotoğraf,
+
+- Elektrik panolarının bulunduğu alanlardan, aydınlatma armatürlerinden, elektrik tesisatından ve jeneratörden geniş açılı birkaç fotoğraf,
+
+- Duman dedektörü kontrol panosundan bir adet fotoğraf,""",
+                    'qsdc_check' : True
+                },
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo',
+                'Fotoğraf yüklemek için tıklayın veya sürükleyip bırakın' : 'photo'
             }
         }
     }
